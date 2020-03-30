99926b65be24fa2a0f1f373e4f0bba858e1952a9
pyPlacca <dhegreatprosperity10@gmail.com>
2020-03-30 01:56:35 +0000

    started telegram bot

--- main.py
+++ main.py
@@ -1,2 +1,39 @@
-import crawler
+import crawler, json
 
+Request = crawler.Request
+urlopen = crawler.urlopen
+
+TG_URL = 'https://api.telegram.org/bot{}'
+paths = {
+    'send': {
+        'message': '/sendMessage?chat_id={chat_id}&text={message}'
+    },
+    'get': '/getUpdates'
+}
+
+
+with open('token.txt', 'r') as tkn:
+    TG_URL = TG_URL.format(tkn.readline())
+
+
+def get_json_data(obj):
+    try:
+        return json.load(obj)
+    except AttributeError:
+        return json.loads(obj)
+
+def get_updates():
+    update = crawler.get_url_response(f"{TG_URL}{paths['get']}")
+    return get_json_data(update)
+
+def send_message(id_, msg):
+    output = paths['send']['message'].format(
+        chat_id=id_, message=msg
+    )
+    return crawler.get_url_response(f"{TG_URL}{output}")
+
+print(get_json_data(
+    crawler.get_url_response(
+        f"{TG_URL}{paths['get']}"
+    )
+))
